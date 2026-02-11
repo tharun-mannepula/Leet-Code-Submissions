cb6515b94f6c2b3d08d4ecc61f518a1936e30503
Mannepula Tharun Kumar <114088094+tharun-mannepula@users.noreply.github.com>
2026-02-11 21:19:20 +0530

    Time: 23 ms (56.24%), Space: 79.4 MB (94.33%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,25 +2,24 @@ class Solution
 {
 public:
     int largestRectangleArea(vector<int> &heights) {
-       int n=heights.size();
-       vector<int> nse(n);
-       vector<int> pse(n);
-       stack<int> st;
-       for(int i=n-1;i>=0;i--){
-        while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
-        nse[i]=st.empty()?n:st.top();
+      int n=heights.size();
+      int maxArea=INT_MIN;
+      stack<int> st;
+      for(int i=0;i<n;i++){
+        while(!st.empty() && heights[st.top()]>=heights[i]){
+            int ele=st.top();
+            st.pop();
+            int pse=st.empty()?-1:st.top();
+            maxArea=max(maxArea,(heights[ele]*(i-pse-1)));
+        }
         st.push(i);
-       }
-       while(!st.empty()) st.pop();
-       for(int i=0;i<n;i++){
-        while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
-        pse[i]=st.empty()?-1:st.top();
-        st.push(i);
-       }
-       int maxArea=0;
-       for(int i=0;i<n;i++){
-        maxArea=max(maxArea,(heights[i]*(nse[i]-pse[i]-1)));
-       }
-       return maxArea;
+      }
+      while(!st.empty()){
+        int ele=st.top();
+        st.pop();
+        int pse=st.empty()?-1:st.top();
+        maxArea=max(maxArea,(heights[ele]*(n-pse-1)));
+      }
+      return maxArea;
     }
 };
