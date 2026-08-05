d9d9b6443d2e0e20dd5660e02db0c4d99097f4e7
Mannepula Tharun Kumar <114088094+tharun-mannepula@users.noreply.github.com>
2026-08-05 15:02:41 +0530

    Time: 11 ms (85.63%), Space: 38.9 MB (19.85%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<pair<int,int>,int>> q;
+        vector<vector<int>> vis(n,vector<int>(m,1));
+        vector<vector<int>> dist(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==0){
+                    q.push({{i,j},0});
+                    vis[i][j]=0;
+                }
+            }
+        }
+        int delRow[]={-1,0,1,0};
+        int delCol[]={0,1,0,-1};
+        while(!q.empty()){
+          int row=q.front().first.first;
+          int col=q.front().first.second;
+          int steps=q.front().second;
+          q.pop();
+          dist[row][col]=steps;
+          for(int i=0;i<4;i++){
+            int nrow=row+delRow[i];
+            int ncol=col+delCol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==1){
+                vis[nrow][ncol]=0;
+                q.push({{nrow,ncol},steps+1});
+            }
+          }
+        }
+        return dist;
+    }
+};
