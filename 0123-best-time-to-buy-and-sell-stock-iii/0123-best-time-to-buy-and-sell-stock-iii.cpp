7c630e27c426ed76b647dd079111585dc4d54275
Mannepula Tharun Kumar <114088094+tharun-mannepula@users.noreply.github.com>
2026-09-02 14:17:02 +0530

    Time: 47 ms (72.17%), Space: 79.3 MB (79.84%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+        int n=arr.size();
+        vector<vector<int>> ahead(2,vector<int>(3,0));
+        vector<vector<int>> curr(2,vector<int>(3,0));
+        for(int ind=n-1;ind>=0;ind--){
+           for(int buy=0;buy<=1;buy++){
+             for(int cap=1;cap<=2;cap++){
+                if(buy){
+                    curr[buy][cap]=max(-arr[ind]+ahead[0][cap],ahead[1][cap]);
+                }
+                else{
+                    curr[buy][cap]=max(arr[ind]+ahead[1][cap-1],ahead[0][cap]);
+                }
+             }
+           }
+           ahead=curr;
+        }
+        return ahead[1][2];
+    }
+};
