e86655bfd2e6c4494a27eeca39c00b26fb36faa1
Mannepula Tharun Kumar <114088094+tharun-mannepula@users.noreply.github.com>
2026-09-02 14:39:29 +0530

    Time: 6 ms (50.94%), Space: 19.4 MB (49.51%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canFinish(int V, vector<vector<int>>& arr) {
+         vector<int> adj[V];
+        for(auto it : arr){
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int> indegree(V,0);
+         queue<int> q;
+        for(int i=0;i<V;i++){
+            for(auto it : adj[i]) indegree[it]++;
+        }
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        if(topo.size()==V) return true;
+        else return false;
+    }
+};
